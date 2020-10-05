42cf834ea6c2df220a585825821ac9cf6206b839
Nusrat Jahan <njnusrat53@gmail.com>
2020-10-05 13:35:45 +0600

    config fix

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.get('/', (req, res) => {
 	res.send('<h2>Volunteer Server is running</h2>');
@@ -19,11 +21,16 @@ app.get('/', (req, res) => {
 
 //database connection
 
-const uri =
-	'mongodb+srv://adminVolunteer:adgjmptw499@cluster0.oawso.mongodb.net/volunteer-network?retryWrites=true&w=majority';
+/* const uri =
+	'mongodb+srv://adminVolunteer:adgjmptw499@cluster0.oawso.mongodb.net/volunteer-network?retryWrites=true&w=majority'; */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oawso.mongodb.net/${process.DB_NAME}?retryWrites=true&w=majority`;
 
 /*****************all categories*******************/
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 client.connect((err) => {
 	const categoriesDB = client.db('volunteer-network').collection('categories');
 	const volunteersDB = client.db('volunteer-network').collection('volunteers');
@@ -42,10 +49,9 @@ client.connect((err) => {
 			if (err) {
 				console.log(err);
 				res.status(500).send({
-					message : err
+					message: err
 				});
-			}
-			else {
+			} else {
 				res.send(documents);
 			}
 		});
@@ -53,12 +59,15 @@ client.connect((err) => {
 
 	//get categories by ID
 	app.get('/categories/:id', (req, res) => {
-		categoriesDB.find({ _id: ObjectId(req.params.id) }).toArray((err, documents) => {
+		categoriesDB.find({
+			_id: ObjectId(req.params.id)
+		}).toArray((err, documents) => {
 			if (err) {
 				console.log(err);
-				res.status(500).send({ message: err });
-			}
-			else {
+				res.status(500).send({
+					message: err
+				});
+			} else {
 				res.send(documents[0]);
 			}
 		});
@@ -74,14 +83,15 @@ client.connect((err) => {
 
 	//find events by email
 	app.get('/my-events/:email', (req, res) => {
-		volunteersDB.find({ email: req.params.email }).toArray((err, documents) => {
+		volunteersDB.find({
+			email: req.params.email
+		}).toArray((err, documents) => {
 			if (err) {
 				console.log(err);
 				res.status(500).send({
-					message : err
+					message: err
 				});
-			}
-			else {
+			} else {
 				res.send(documents);
 			}
 		});
@@ -89,7 +99,9 @@ client.connect((err) => {
 
 	//delete an event by id
 	app.delete('/delete/:id', (req, res) => {
-		volunteersDB.deleteOne({ _id: ObjectId(req.params.id) }).then((result) => {
+		volunteersDB.deleteOne({
+			_id: ObjectId(req.params.id)
+		}).then((result) => {
 			res.send(result.deletedCount > 0);
 		});
 	});
@@ -99,10 +111,9 @@ client.connect((err) => {
 		volunteersDB.find().toArray((err, documents) => {
 			if (err) {
 				res.status(500).send({
-					message : err
+					message: err
 				});
-			}
-			else {
+			} else {
 				res.send(documents);
 			}
 		});
